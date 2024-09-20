6e1b861af6fee21d8242392e8f56c76479c144a9
Latoya930 <latoya@lortechnologies.com>
2024-09-20 10:10:04 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; 
-
-
-createApp(App)
-  .use(router) 
-  .mount('#app');
-=======
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
@@ -19,4 +9,3 @@ app.use(router);
 app.use(createPinia());
 
 createApp(App).mount('#app')
->>>>>>> f76e4f398d0ce868c31699923340a52974671b92
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
-<<<<<<< HEAD
-import Registration from '../views/Registration.vue';
-import Login from '../views/Login.vue';
-import Dashboard from '../views/Dashboard.vue';
-=======
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from './views/Register.vue';
->>>>>>> f76e4f398d0ce868c31699923340a52974671b92
 
 
 const routes = [
   { path: '/', name: 'Login', component: Login },
-<<<<<<< HEAD
-  { path: '/register', name: 'Registration', component: Registration },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-];
-
-
-=======
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/register', name: 'Login', component: Register },
 ];
 
->>>>>>> f76e4f398d0ce868c31699923340a52974671b92
 const router = createRouter({
   history: createWebHistory(),
   routes,
