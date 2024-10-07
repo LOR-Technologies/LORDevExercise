b55772b25b40de512321bf23f20a24cd5b4f4007
Oratile <oratile@lortechnologies.com>
2024-10-07 08:17:39 +0200

    adding login controller

--- Kota Project/api/src/controllers/user.controller.ts
+++ Kota Project/api/src/controllers/user.controller.ts
@@ -1,6 +1,33 @@
 import bcrypt from "bcrypt";
 import usersTable from "../schemas/user.schema";
 import db from "../config/db";
+import { eq }  from 'drizzle-orm';
+
+const loginUserController = async (c: any) => {
+  try {
+    const { email, password } = await c.req.json(); // Parse request body
+
+    const userInBD = await db.select().from(usersTable).where(eq(usersTable.email, email)).limit(1);
+
+    if (!userInBD) {
+      return c.json({ message: 'Cannot find user' }, 400);
+    }
+
+    // Compare password with hashed password
+    const isPasswordCorrect = await bcrypt.compare(password, userInBD[0].password as string);
+
+    if (!isPasswordCorrect) {
+      return c.json({ message: 'Unsuccessful login' });
+    }
+
+    //generate jwt
+
+    return c.json({ message: 'Login successful' });
+  } catch (error) {
+    console.error('Error during login:', error);
+    return c.json({ message: 'An error occurred during login' }, 500);
+  }
+};
 
 const createUserController = async (c: any) => {
   try {
@@ -24,4 +51,6 @@ const createUserController = async (c: any) => {
   }
 };
 
-export { createUserController };
+
+
+export { createUserController, loginUserController };
